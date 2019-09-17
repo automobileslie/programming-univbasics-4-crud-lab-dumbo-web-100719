39bfd943dab2e70059500b5034f4cd9d2bd00955
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-17 01:57:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  new_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  new_array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
